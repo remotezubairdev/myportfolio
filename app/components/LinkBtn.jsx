55e26a93774d4b389ed59c0309024cb26f911649
Zubair Dev <164741377+remotezubairdev@users.noreply.github.com>
2026-08-05 23:09:37 +0500

    Update LinkBtn.jsx

--- app/components/LinkBtn.jsx
+++ app/components/LinkBtn.jsx
@@ -5,7 +5,7 @@ const LinkBtn = ({ text, path, variant, icon }) => {
   return (
     <Link
     href={path}
-    className={`${variant == 2 ? "border border-gray-700 hover:bg-gray-700" : "bg-white text-black hover:bg-gray-200"} flex items-center gap-2 transition font-bold rounded-xl text-xs sm:text-md md:text-lg px-2 md:px-4 py-2 md:py-2`}
+    className={`${variant == 2 ? "border border-gray-700 hover:bg-gray-700" : "bg-white text-black hover:bg-gray-200"} flex items-center gap-2 transition font-bold rounded-xl text-[10px] sm:text-md md:text-lg px-2 md:px-4 py-2 md:py-2`}
     >
         {icon}
         {text}
